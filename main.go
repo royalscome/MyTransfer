04b04fa772179bfb95429218e284088042dd5114
魏洋 <1210532312@qq.com>
2024-03-19 16:34:40 +0800

    fix: 修复多网络端口无法过滤本机消息问题

--- main.go
+++ main.go
@@ -3,37 +3,96 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 	"time"
 )
 
 var (
-	myDeviceIpv4Address string
+	myDeviceIpv4Address []string
 )
 
-func getMyDeviceIP() (string, error) {
-	addrs, err := net.InterfaceAddrs()
+//	func getMyDeviceIP() (string, error) {
+//		//addrs, err := net.InterfaceAddrs()
+//		//if err != nil {
+//		//	fmt.Println(err)
+//		//	return "", err
+//		//}
+//		//
+//		//for _, addr := range addrs {
+//		//	fmt.Println(addr)
+//		//	if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+//		//		if ipnet.IP.To4() != nil {
+//		//			fmt.Println("IPv4: ", ipnet.IP.String())
+//		//			//return ipnet.IP.String(), nil
+//		//		}
+//		//	}
+//		//}
+//		interfaces, err := net.Interfaces()
+//		if err != nil {
+//			return "", err
+//		}
+//
+//		for _, i := range interfaces {
+//			fmt.Println(i.Name)
+//			if i.Name != "eth0" {
+//				continue
+//			}
+//
+//			addrs, err := i.Addrs()
+//			if err != nil {
+//				return "", err
+//			}
+//
+//			for _, addr := range addrs {
+//				if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+//					if ipnet.IP.To4() != nil {
+//						return ipnet.IP.String(), nil
+//					}
+//				}
+//			}
+//		}
+//		return "", fmt.Errorf("your device not has IPv4")
+//	}
+func getMyDeviceIP() ([]string, error) {
+	var ips []string
+	interfaces, err := net.Interfaces()
 	if err != nil {
-		fmt.Println(err)
-		return "", err
+		return nil, err
 	}
 
-	for _, addr := range addrs {
-		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				fmt.Println("IPv4: ", ipnet.IP.String())
-				return ipnet.IP.String(), nil
+	for _, i := range interfaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			return nil, err
+		}
+
+		for _, addr := range addrs {
+			if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					ip := ipnet.IP.String()
+					if strings.HasPrefix(ip, "192.168") {
+						ips = append(ips, ip)
+					}
+				}
 			}
 		}
 	}
-	return "", fmt.Errorf("your device not has IPv4")
+
+	if len(ips) == 0 {
+		return nil, fmt.Errorf("no IPv4 address starting with 192.168 found")
+	}
+
+	return ips, nil
 }
 
 func main() {
-	device, err := getMyDeviceIP()
+	devices, err := getMyDeviceIP()
 	if err != nil {
 		return
 	}
-	myDeviceIpv4Address = fmt.Sprintf("%s:%s", device, "8080")
+	for _, dev := range devices {
+		myDeviceIpv4Address = append(myDeviceIpv4Address, fmt.Sprintf("%s:8080", dev))
+	}
 	if err != nil {
 		panic(err)
 	}
@@ -61,7 +120,17 @@ func main() {
 				return
 			}
 			//fmt.Println(remoteAddr, myDeviceIpv4Address)
-			if remoteAddr.String() == myDeviceIpv4Address {
+			// 如果接收到的消息是本机发送的消息，则不处理
+			// 循环比对本机的所有IP地址，如果接收到的消息是本机的IP地址，则不处理
+			// 如果接收到的消息不是本机的IP地址，则处理
+			var isMyDevice bool
+			for _, address := range myDeviceIpv4Address {
+				if remoteAddr.String() == address {
+					isMyDevice = true
+				}
+			}
+
+			if isMyDevice {
 				fmt.Println("接收到本机消息")
 			} else {
 				fmt.Printf("Received from address: %s data: %s\n", remoteAddr, data[:n])
